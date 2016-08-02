8f84911f39d617c10859bec709697d68b5e6b9d8
sebastianrjay <sebastianrjay@gmail.com>
2016-08-02 12:13:21 -0700

    Abstract route error handling and request validation

--- api/saved_url_content/router.js
+++ api/saved_url_content/router.js
@@ -5,41 +5,46 @@ const SavedUrlContent = require('../../models/saved_url_content');
 
 const queue = kue.createQueue();
 
-const requirePresenceOfUrl = (req, next) => {
-	if(!req.body.url) next(new Error('Request body must contain url'));
+const ensurePresenceOfUrl = () => {
+	return (req, res, next) => {
+		if(!req.body.url) {
+			return res.status(400).json({ error: 'Request body must contain url' });
+		}
+
+		next();
+	}
 };
 
-// View all SavedUrlContent docs as JSON
+const handleErr = (res, message, code) => {
+  console.error("ERROR: " + message);
+  res.status(code || 500).json({ error: message });
+};
+
+// View all SavedUrlContent docs as JSON; optionally pass URL for specific doc
 router.get('/', (req, res, next) => {
 	const dbQuery = (req.body.url ? { url: req.body.url } : {});
-
-	SavedUrlContent.find(dbQuery, (err, savedUrlContents) => {
-		if(err) return next(err);
-
-		res.status(200).json(savedUrlContents);
-	});
+	
+	SavedUrlContent.find(dbQuery).exec().then(
+		savedUrlContents => res.status(200).json(savedUrlContents),
+		err => handleErr(res, err.message)
+	);
 });
 
 // View single SavedUrlContent doc as JSON, by URL
-router.post('/results', (req, res, next) => {
-	requirePresenceOfUrl(req, next);
-
-	SavedUrlContent.findOne({ url: req.body.url }, (err, savedUrlContent) => {
-		if(err) return next(err);
-
-		res.status(200).json(savedUrlContent);
-	});
+router.post('/results', ensurePresenceOfUrl(), (req, res, next) => {
+	SavedUrlContent.findOne({ url: req.body.url }).exec().then(
+		savedUrlContent => res.status(200).json(savedUrlContent),
+		err => handleErr(res, err.message)
+	);
 });
 
 // View job status corresponding to a SavedUrlContent doc, by URL
-router.post('/status', (req, res, next) => {
-	requirePresenceOfUrl(req, next);
-
+router.post('/status', ensurePresenceOfUrl(), (req, res, next) => {
 	SavedUrlContent.findOne({ url: req.body.url }, (err, savedUrlContent) => {
-		if(err) return next(err);
+		if(err) return handleErr(res, err.message);
 
 		kue.Job.get(savedUrlContent.jobId, (err, job) => {
-	    if(err) return next(err);
+	    if(err) return handleErr(res, err.message);
 
 	    res.status(200).json(job);
 	  });
@@ -51,30 +56,29 @@ router.post('/status', (req, res, next) => {
 // the app will create a new 'save url' job instance with that URL, fetch the 
 // HTML again, and later findOneAndUpdate the corresponding doc (or create it if
 // it doesn't exist).
-router.post('/', (req, res, next) => {
-	requirePresenceOfUrl(req, next);
+router.post('/', ensurePresenceOfUrl(), (req, res, next) => {
+	let errorOccurred = false;
 
 	const job = queue.create('save url', {
 		url: req.body.url
 	}).priority(req.body.priority || 'high')
 		.attempts(req.body.attempts || 3)
-		.save(err => {
-			if(err) return next(err);
-	});
+		.save((err, job) => {
+			if(err) errorOccurred = true;
+		});
 
-	res.status(200).json(job);
+	if(errorOccurred) handleErr(res, "Unable to create new 'save url' job");
+	else res.status(200).json(job);
 });
 
 // Delete a SavedUrlContent doc (and its corresponding job) by URL (hook deletes job)
-router.delete('/', (req, res, next) => {
-	requirePresenceOfUrl(req, next);
-
+router.delete('/', ensurePresenceOfUrl(), (req, res, next) => {
 	SavedUrlContent.findOne({ url: req.body.url }, (err, savedUrlContent) => {
-		if(err) return next(err);
+		if(err) return handleErr(res, err.message);
 
 		savedUrlContent.remove().then(
 			removedDoc => res.status(200).json(removedDoc),
-			err => next(err)
+			err => handleErr(res, err.message)
 		);
 	})
 });
